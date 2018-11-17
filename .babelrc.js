1e50ef96ce498b4a5b06a6b533dd954994111d41
David Gilbertson <gilbertson.david@gmail.com>
2018-11-17 13:07:24 +1100

    More targeted Babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,20 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          // Proxy was introduced in these browser versions
+          chrome: '58',
+          edge: '12',
+          firefox: '18',
+          safari: '10',
+          ios: '10',
+        },
+      },
+    ],
     '@babel/preset-react',
   ],
+  comments: false,
+  minified: true,
 };
