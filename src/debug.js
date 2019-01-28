49e25f2fd848a19457874ae8c7bbd0f487ce5f2c
Richard van der Dys <richard.vanderdys@gmail.com>
2019-01-28 05:26:40 +0200

    Safer debug check (#58)
    * Safer debug check
    
    Handling environments like JSDOM which have `window` but no `localStorage`
    
    * Fixed check
    
    * Updated variable name

--- src/debug.js
+++ src/debug.js
@@ -1,20 +1,20 @@
 const DEBUG_ON = 'on';
 const DEBUG_OFF = 'off';
 
-const isInBrowser = typeof window !== 'undefined';
+const hasLocalStorage = typeof window !== 'undefined' && window.localStorage !== undefined;
 
-let DEBUG = isInBrowser
+let DEBUG = hasLocalStorage
   ? (window.localStorage.RECOLLECT__DEBUG || DEBUG_OFF)
   : DEBUG_OFF;
 
 export const debugOn = () => {
   DEBUG = DEBUG_ON;
-  if (isInBrowser) window.localStorage.RECOLLECT__DEBUG = DEBUG;
+  if (hasLocalStorage) window.localStorage.RECOLLECT__DEBUG = DEBUG;
 };
 
 export const debugOff = () => {
   DEBUG = DEBUG_OFF;
-  if (isInBrowser) window.localStorage.RECOLLECT__DEBUG = DEBUG;
+  if (hasLocalStorage) window.localStorage.RECOLLECT__DEBUG = DEBUG;
 };
 
 export const isDebugOn = () => DEBUG === DEBUG_ON;
