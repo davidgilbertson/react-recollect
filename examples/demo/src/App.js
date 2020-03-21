f5c44fd0716d2a2ffaaa687d704140d39b7a4dc4
David Gilbertson <gilbertson.david@gmail.com>
2020-03-21 18:40:15 +1100

    Add demo site shell

---
+++ examples/demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Recollect demo site</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
