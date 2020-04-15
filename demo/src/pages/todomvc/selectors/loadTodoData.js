4fbf8f1d1f7f325f9093332c046f52da89d2faa6
David Gilbertson <gilbertson.david@gmail.com>
2020-04-15 12:01:29 +1000

    Use dummy data in demo site

--- demo/src/pages/todomvc/selectors/loadTodoData.js
+++ demo/src/pages/todomvc/selectors/loadTodoData.js
@@ -2,16 +2,47 @@ import { store } from 'react-recollect';
 import { LOAD_STATUSES } from '../../../shared/constants';
 
 /** @return void */
-const loadTodoData = async () => {
+const loadTodoData = () => {
   store.todoMvcPage.loadStatus = LOAD_STATUSES.LOADING;
 
-  const todos = await fetch(
-    'https://jsonplaceholder.typicode.com/todos'
-  ).then((response) => response.json());
+  // Simulate async fetching
+  setTimeout(() => {
+    store.todoMvcPage.todos = [
+      {
+        userId: 1,
+        id: 1,
+        title: 'delectus aut autem',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: 'quis ut nam facilis et officia qui',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: 'fugiat veniam minus',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: 'et porro tempora',
+        completed: true,
+      },
+      {
+        userId: 1,
+        id: 5,
+        title:
+          'laboriosam mollitia et enim quasi adipisci quia provident illum',
+        completed: false,
+      },
+    ];
 
-  // Take the first 5 todos (there's 200)
-  store.todoMvcPage.todos = todos.slice(0, 5);
-  store.todoMvcPage.loadStatus = LOAD_STATUSES.LOADED;
+    store.todoMvcPage.loadStatus = LOAD_STATUSES.LOADED;
+  }, 50);
 };
 
 export default loadTodoData;
